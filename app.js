7be72d912f212c9e9eeee6f0932dc9a9b35e17e3
Carl Lindman <carl.a.lindman@gmail.com>
2022-03-16 14:46:00 +0100

    crud-template

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("express-async-errors");
+
+// utils
+const config = require("./utils/config");
+// routers
+const highscoresRouter = require("./controllers/highscores");
+
+const app = express(); // initialize express app
+
+console.log("connecting to", config.MONGODB_URI); // log mongodb uri
+
+// connect to mongodb
+mongoose
+  .connect(config.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("error connection to MongoDB:", error.message);
+  });
+
+// middleware
+app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
+
+// routers
+app.use("/api/highscores", highscoresRouter);
+
+module.exports = app;
