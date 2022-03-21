03d358d0e2bc8e698fbd834c4537c763aad08f45
Carl Lindman <carl.a.lindman@gmail.com>
2022-03-21 14:34:07 +0100

    add /* router to server

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 require("express-async-errors");
 
 // utils
