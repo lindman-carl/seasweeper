517e7075706fe24a39daea74042176b1022dcee3
Carl Lindman <carl.a.lindman@gmail.com>
2022-03-21 14:29:44 +0100

    add /* router to server

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ app.use(express.static("build"));
 app.use(express.json());
 
 // routers
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 app.use("/api/highscores", highscoresRouter);
 
 module.exports = app;
