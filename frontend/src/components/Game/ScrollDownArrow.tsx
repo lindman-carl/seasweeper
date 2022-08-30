bc3e55ce7829792243d159b6f2191464abb5db80
yoga-python <73431769+yoga-python@users.noreply.github.com>
2022-08-31 01:02:06 +0200

    add tooltip to scroll down arrow

--- frontend/src/components/Game/ScrollDownArrow.tsx
+++ frontend/src/components/Game/ScrollDownArrow.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 // icons
 import { RiArrowDownSLine } from "react-icons/ri";
+import ReactTooltip from "react-tooltip";
 
 const ScrollDownArrow = () => {
   const scrollToHighscoreList = () =>
@@ -22,9 +23,20 @@ const ScrollDownArrow = () => {
         cursor-pointer"
       onClick={scrollToHighscoreList}
     >
-      <div className="my-4 animate-pulse text-sky-700">
+      <div
+        className="my-4 animate-pulse text-sky-700"
+        data-for="down-arrow"
+        data-tip="Scroll down for highscorelist"
+      >
         <RiArrowDownSLine size={24} />
       </div>
+      <ReactTooltip
+        id="down-arrow"
+        type="info"
+        effect="solid"
+        delayShow={600}
+        place="top"
+      />
     </div>
   );
 };
