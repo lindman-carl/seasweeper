dae094a8301097f3a21c58421d824aebeec96b73
yoga-python <73431769+yoga-python@users.noreply.github.com>
2022-08-31 00:36:40 +0200

    update highscore entry time precision

--- frontend/src/components/Highscore/HighscoreListRow.tsx
+++ frontend/src/components/Highscore/HighscoreListRow.tsx
@@ -35,7 +35,7 @@ const HighScoreRow = ({ highscore: { playerName, time, rank } }: Props) => {
     <>
       <div className="highscores-list-rank">{rankFormatted()}</div>
       <div className="highscores-list-name">{playerName}</div>
-      <div className="highscores-list-time">{(time / 1000).toFixed(2)}</div>
+      <div className="highscores-list-time">{(time / 1000).toPrecision()}</div>
     </>
   );
 };
