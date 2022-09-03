3e085848e7066b4dca037e4e295ff1ddf415ec8e
yoga-python <73431769+yoga-python@users.noreply.github.com>
2022-09-03 05:37:23 +0200

    uibuild

---
+++ frontend/src/context/themeContext.tsx
@@ -0,0 +1,17 @@
+export const themes = [
+  {
+    id: 0,
+    name: "light",
+    colors: {
+      background: "#BAE6FD",
+      foreground: "#F0F9FF",
+      text: "#0C4A6E",
+      border: "#0069A1",
+      purple: "#A855F7",
+      tileBorder: "#D1D5DB",
+      tileBorderHover: "#64748B",
+      decoration: "#93C5FD",
+      soft: "#0284C7",
+    },
+  },
+];
