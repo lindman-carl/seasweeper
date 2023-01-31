e74fcb837cd20d923051a43031b5cbce7aa20913
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-02-01 00:06:47 +0100

    add gamemode carousel e2e tests

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -23,24 +23,6 @@ describe("The game", () => {
     }
   });
 
-  it("can win the game", () => {
-    const tilesToClick = [
-      0, 80, 64, 8, 12, 159, 396, 386, 267, 220, 82, 4, 5, 50, 49, 69, 68, 88,
-      128, 148, 147, 168, 169, 188, 209, 295, 294, 293, 313, 314, 312, 311, 291,
-      134, 114, 115, 94, 93, 74, 75, 17, 15, 327, 328, 349, 389, 242, 243, 202,
-    ];
-
-    // click all the tiles
-    for (const id of tilesToClick) {
-      cy.get(`#tile-${id}`).click();
-    }
-
-    // expect the game to end
-    cy.get(".gameoverbox-container").should("be.visible");
-    // check the header contains any number cypress
-    cy.get(".gameoverbox-header").contains(/\d+/s);
-  });
-
   describe("clicking tile", () => {
     it("reveals the correct tiles", () => {
       cy.get("#tile-0").find(".tile-clickable").click();
@@ -66,12 +48,12 @@ describe("The game", () => {
 
         // a tile is revealed if it has the revealed background color: bg-sky-50
         if (expectedRevealed.hasOwnProperty(id)) {
-          cy.get(`#tile-${id}`)
+          cy.get(`#tile-${id}`, { log: false })
             .find("div")
             .should("have.class", "tile-base")
             .should("have.class", "bg-sky-50");
         } else {
-          cy.get(`#tile-${id}`)
+          cy.get(`#tile-${id}`, { log: false })
             .find("div")
             .should("have.class", "tile-base")
             .should("not.have.class", "bg-sky-50");
@@ -164,6 +146,7 @@ describe("The game", () => {
           .should("not.have.class", "bg-yellow-100");
       }
     });
+
     it("can place 2 lighthouses", () => {
       // expect 2 lighthouses left
       cy.get("[data-id=toggle-lighthouse-mode]").contains("2");
@@ -220,6 +203,123 @@ describe("The game", () => {
 
       // expect to still have 2 lighthouses left
       cy.get("[data-id=toggle-lighthouse-mode]").contains("2");
+    });
+  });
+
+  describe("gamemode carousel", () => {
+    it("can toggle the carousel", () => {
+      // expect the carousel to be hidden
+      cy.get(".carousel-container").should("not.exist");
+
+      // toggle the carousel, expect it to be visible
+      cy.get("[data-id=toggle-gamemode-carousel]").click();
+      cy.get(".carousel-container").should("be.visible");
+
+      // toggle the carousel again, expect it to be hidden again
+      cy.get("[data-id=toggle-gamemode-carousel]").click();
+      cy.get(".carousel-container").should("not.exist");
+    });
+
+    it("can select a gamemode", () => {
+      // toggle the carousel
+      // select a gamemode
+      cy.get("[data-id=toggle-gamemode-carousel]").click();
+      cy.get("#carousel-right-button").click();
+      // select Kiddie pool
+      cy.get(".carousel-header").contains("Kiddie pool");
+      cy.get(".carousel-card").click();
+
+      // expect the carousel to close
+      cy.get(".carousel-container").should("not.exist");
+
+      // expect 100 sea tiles
+      cy.get(".tile-base.bg-blue-300").should("have.length", 100);
+
+      // toggle the carousel
+      // select a gamemode
+      cy.get("[data-id=toggle-gamemode-carousel]").click();
+      cy.get("#carousel-left-button").click();
+      cy.get("#carousel-left-button").click();
+      // select Continent
+      cy.get(".carousel-header").contains("Continent");
+      cy.get(".carousel-card").click();
+
+      // expect the carousel to close
+      cy.get(".carousel-container").should("not.exist");
+
+      // expect a mix of sea and land tiles
+      cy.get(".tile-base.bg-blue-300").should("have.length.at.least", 100);
+      cy.get(".tile-base.bg-green-300").should("have.length.at.least", 50);
+    });
+
+    it("can close the carousel by clicking the toggle button", () => {
+      // toggle the carousel
+      cy.get("[data-id=toggle-gamemode-carousel]").click();
+      cy.get(".carousel-container").should("be.visible");
+
+      // close the carousel by clicking the toggle button
+      cy.get("[data-id=toggle-gamemode-carousel]").click();
+      cy.get(".carousel-container").should("not.exist");
+    });
+
+    it("can close the carousel by clicking the close button", () => {
+      // toggle the carousel
+      cy.get("[data-id=toggle-gamemode-carousel]").click();
+      cy.get(".carousel-container").should("be.visible");
+
+      // close the carousel by clicking the close button
+      cy.get(".carousel-close-button").click();
+      cy.get(".carousel-container").should("not.exist");
+    });
+
+    it("can regenerate the board", () => {
+      // toggle the carousel
+      cy.get("[data-id=toggle-gamemode-carousel]").click();
+      cy.get(".carousel-container").should("be.visible");
+
+      // compare the number of sea tiles in the old board and the new board
+      // most of the time, the number of sea tiles should be different
+      cy.get(".tile-carousel-base.bg-blue-300").then(($tiles) => {
+        // regenerate the board,
+        // expect the tiles to be different from the old tiles
+        cy.get("#carousel-regenerate-button").click();
+        cy.get(".tile-carousel-base.bg-blue-300").should(
+          "have.length.not.equal",
+          $tiles.length
+        );
+      });
+    });
+
+    it("can't interact with the board when the carousel is open", () => {
+      // toggle the carousel
+      cy.get("[data-id=toggle-gamemode-carousel]").click();
+      cy.get(".carousel-container").should("be.visible");
+
+      // click a tile
+      cy.get("#tile-0").find(".tile-clickable").click();
+      // expect the tile to not be revealed
+      cy.get("#tile-0").find(".tile-base").should("have.class", "bg-blue-300");
+    });
+  });
+
+  describe("playing the game", () => {
+    it("can win the game", () => {
+      const tilesToClick = [
+        0, 80, 64, 8, 12, 159, 396, 386, 267, 220, 82, 4, 5, 50, 49, 69, 68, 88,
+        128, 148, 147, 168, 169, 188, 209, 295, 294, 293, 313, 314, 312, 311,
+        291, 134, 114, 115, 94, 93, 74, 75, 17, 15, 327, 328, 349, 389, 242,
+        243, 202,
+      ];
+
+      // click all the tiles
+      for (const id of tilesToClick) {
+        cy.get(`#tile-${id}`).click();
+      }
+
+      // expect the game to end
+      cy.get(".gameoverbox-container").should("be.visible");
+      // check the header contains any number cypress
+      cy.get(".gameoverbox-header").contains(/\d+/s);
     });
   });
 });
