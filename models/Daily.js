4e8cb639706b90bfeaa2e0f40e08fa3919e790da
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-02-03 00:45:57 +0100

    add Daily map schema

---
+++ models/Daily.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const dailySchema = mongoose.Schema({
+  map: {
+    type: String,
+    required: true,
+  },
+  width: {
+    type: Number,
+    required: true,
+  },
+  height: {
+    type: Number,
+    required: true,
+  },
+  dateString: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Number,
+    required: true,
+  },
+});
+
+dailySchema.set("toJSON", {
+  transform: (_, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Daily = mongoose.model("Daily", dailySchema);
+
+module.exports = Daily;
