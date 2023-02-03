6f14d4d5318bebd7b0a60989a255a57e545551d1
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-02-03 15:20:39 +0100

    update game text sea

--- frontend/src/components/Game/GameText.tsx
+++ frontend/src/components/Game/GameText.tsx
@@ -7,7 +7,7 @@ const GameText = () => (
   <div className="game-text-container">
     Click on a blue (sea tile) tile to reveal it. Flags are for slow players,
     try to mark the mines in your head. Place lighthouses on green tiles (land
-    tiles) to safely reveal adjacent water tiles.
+    tiles) to safely reveal adjacent sea tiles.
     <a
       className="game-text-link"
       href="https://minesweepergame.com/strategy.php"
