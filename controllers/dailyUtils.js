cec36147cea50ed05bc1931d7b9b5ebf7419ff9b
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-02-05 00:12:59 +0100

    fix cjs exports

--- controllers/dailyUtils.js
+++ controllers/dailyUtils.js
@@ -1,6 +1,6 @@
 // combinations of island generation parameters
 // [nIslands, clusterSpread]
-export const generationCombos = [
+const generationCombos = [
   [3, 10],
   [6, 2],
   [6, 4],
@@ -8,14 +8,14 @@ export const generationCombos = [
   [10, 4],
   [20, 2],
 ];
-export const lighthouseOptions = [0, 1, 2, 3];
+const lighthouseOptions = [0, 1, 2, 3];
 
 // get a random element from an array
-export const randomElement = (array) =>
+const randomElement = (array) =>
   array[Math.floor(Math.random() * array.length)];
 
 // flatten 2d map to string
-export const mapToString = (map) => {
+const mapToString = (map) => {
   let results = "";
   for (const row of map) {
     let rowString = row.join("");
@@ -29,7 +29,17 @@ const zeroPad = (num) => (num < 10 ? `0${num}` : num);
 
 // get the current date as a string
 // "YYYY-MM-DD" zero-padded, e.g. "2020-01-01", day and month is 1-indexed
-export const getDateString = (date) =>
+const getDateString = (date) =>
   `${date.getFullYear()}-${zeroPad(date.getMonth() + 1)}-${zeroPad(
     date.getDate()
   )}`;
+
+// export all the functions
+module.exports = {
+  generationCombos,
+  lighthouseOptions,
+  randomElement,
+  mapToString,
+  zeroPad,
+  getDateString,
+};
